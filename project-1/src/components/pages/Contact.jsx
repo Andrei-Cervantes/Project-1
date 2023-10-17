8ed79e79ac9a7cb0c57dda2d3a6416e003cfe02f
Andrei-Cervantes <andrei29.cervantes@gmail.com>
2023-10-17 19:09:58 +0800

    Fixing Contact Page

--- project-1/src/components/pages/Contact.jsx
+++ project-1/src/components/pages/Contact.jsx
@@ -14,41 +14,50 @@ import { BsFillTelephoneFill } from "react-icons/bs";
 export const Contact = () => {
   return (
     <>
-      <div className="hero-section">
-        <div className="block"></div>
-        <h1 className="text-center">GET IN TOUCH WITH US!</h1>
-      </div>
-      <Container fluid>
-        <Row className="contact-container">
-          <Col className="contact-maps">
-
-          </Col>
-          <Col className="contact-details">
-            <h2>Our Contact Details:</h2>
-            <Row className="footer-contact">
-              <a className="footer-link" href="mailto:padesprinting@yahoo.com">
-                <AiFillMail className="footer-icon" />
-                padesprinting@yahoo.com
-              </a>
-            </Row>
-            <Row className="footer-contact">
-              <a className="footer-link" href="https://www.facebook.com/PADESprintingServices" target="blank">
-                <AiFillFacebook className="footer-icon" />
-                PADES Printing Services
-              </a>
-            </Row>
-            <Row className="footer-contact">
-              <a className="footer-link" href="tel:+(02)8.659-4131">
-                <BsFillTelephoneFill className="footer-icon" />
-                (02)8.659-4131
-              </a>
-            </Row>
-          </Col>
-          <Col className="contact-address">
-
-          </Col>
-        </Row>
-      </Container>
+      <section className="contact-banner">
+        <Container className="contact-content">
+          <Row>
+            <h2>GET IN TOUCH WITH US!</h2>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <div className="contact-container">
+          <div className="contact-details">
+            <h3 className="contact-title">Our contact details:</h3>
+            <div className="contact-info">
+              <Col>
+                <Row className="contact">
+                  <a
+                    className="footer-link"
+                    href="mailto:padesprinting@yahoo.com"
+                  >
+                    <AiFillMail className="footer-icon" />
+                    padesprinting@yahoo.com
+                  </a>
+                </Row>
+                <Row className="contact">
+                  <a
+                    className="footer-link"
+                    href="https://www.facebook.com/PADESprintingServices"
+                    target="blank"
+                  >
+                    <AiFillFacebook className="footer-icon" />
+                    PADES Printing Services
+                  </a>
+                </Row>
+                <Row className="contact">
+                  <a className="footer-link" href="tel:+(02)8.659-4131">
+                    <BsFillTelephoneFill className="footer-icon" />
+                    (02)8.659-4131
+                  </a>
+                </Row>
+              </Col>
+            </div>
+          </div>
+          <div className="contact-locations"></div>
+        </div>
+      </section>
     </>
   );
 };
